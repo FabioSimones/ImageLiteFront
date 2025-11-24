9a0b587a9f98c3bf5acb43046da1ba2d408e293f
FabioSimones <simones.fabio1994@gmail.com>
2025-11-24 18:00:53 -0300

    Criando o template de páginas.

---
+++ src/app/galeria/page.tsx
@@ -0,0 +1,10 @@
+import { Template } from '../../components/Template'
+
+export default function GaleriaPage(){
+    return(
+        <Template>
+            <h1>Galeria</h1>
+        </Template>
+    
+    )
+}
