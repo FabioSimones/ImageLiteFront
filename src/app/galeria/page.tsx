15cfb914ef6775e3aaca85766a7a85018bfde2df
FabioSimones <simones.fabio1994@gmail.com>
2025-11-24 18:26:13 -0300

    Componente ImageCard e Página de galeria.

--- src/app/galeria/page.tsx
+++ src/app/galeria/page.tsx
@@ -1,9 +1,11 @@
-import { Template } from '../../components/Template'
+import { Template, ImageCard } from '@/components'
 
 export default function GaleriaPage(){
     return(
         <Template>
-            <h1>Galeria</h1>
+            <section className='grid grid-cols-3 gap-8'>
+                <ImageCard nome='Cachoeira' tamanho='10MB' dataUpload='24/11/2025' src='https://blog.venturas.com.br/wp-content/uploads/2016/08/costa-rica-a-opcao-ideal-para-o-turismo-de-natureza-na-america-latina.jpeg'/>
+            </section>            
         </Template>
     
     )
