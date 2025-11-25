704615a15f84febff8ecf0eb464166e6b00fd2bf
FabioSimones <simones.fabio1994@gmail.com>
2025-11-25 18:23:24 -0300

    Trazendo imagens através do frontend.

--- src/app/galeria/page.tsx
+++ src/app/galeria/page.tsx
@@ -1,10 +1,29 @@
+'use client'
+
 import { Template, ImageCard } from '@/components'
+import { Image } from '@/resources/image/image.resource';
+import { useImageService } from '@/resources/image/image.service'
+import { useState } from 'react'
 
 export default function GaleriaPage(){
+
+    const userService = useImageService();
+    const [images, setImages] = useState<Image[]>([]);
+
+
+    async function searchImages(){
+        const result = await userService.buscar();
+        setImages(result);
+        console.table(result);
+    }
+
     return(
         <Template>
+
+            <button className='bg-gray-500' onClick={searchImages}>Clique aqui para mudar</button>
+
             <section className='grid grid-cols-3 gap-8'>
-                <ImageCard nome='Cachoeira' tamanho='10MB' dataUpload='24/11/2025' src='https://blog.venturas.com.br/wp-content/uploads/2016/08/costa-rica-a-opcao-ideal-para-o-turismo-de-natureza-na-america-latina.jpeg'/>
+                <ImageCard nome="{nomeImagem}" tamanho='10MB' dataUpload='24/11/2025' src=""/>
             </section>            
         </Template>
     
