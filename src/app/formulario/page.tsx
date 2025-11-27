99cdd3673e3aaf16398c6b69d6aa72d6a398bc06
FabioSimones <simones.fabio1994@gmail.com>
2025-11-27 17:42:01 -0300

    Configurando navegação entre páginas.

--- src/app/formulario/page.tsx
+++ src/app/formulario/page.tsx
@@ -1,5 +1,11 @@
+import { Template } from "@/components"
+
+
 export default function FormularioPage(){
     return(
-        <h1>Formulário</h1>
+        <Template>
+            <h1 className="bg-gray-900">Formulário</h1>
+        </Template>
+        
     )
 }
