29e92076fd5b017bb341c9e73154de2530d417b2
FabioSimones <simones.fabio1994@gmail.com>
2025-12-10 17:52:25 -0300

    Obtendo dados para validar sessão do usuário

--- src/resources/user/authentication.service.ts
+++ src/resources/user/authentication.service.ts
@@ -60,6 +60,31 @@ class AuthService{
     setUserSession (userSessionToken: UserSessionToken){
         localStorage.setItem(AuthService.AUTH_PARAM, JSON.stringify(userSessionToken))
     }
+
+    getUserSession() : UserSessionToken | null {
+        const authString = localStorage.getItem(AuthService.AUTH_PARAM);
+        if(!authString){
+            return null;
+        }
+
+        const token: UserSessionToken = JSON.parse(authString);
+        return token;
+    }
+
+    isSessionValid(): boolean{
+        const userSession: UserSessionToken | null =  this.getUserSession();
+        if(!userSession){
+            return false;
+        }
+
+        const expiration: number | undefined = userSession.expiration;
+        if(expiration){
+            const expirationDateInMillis = expiration * 1000;
+            return new Date() < new Date(expirationDateInMillis)
+        }
+        
+        return false;
+    }
 }
 
 export const userAuth = () => new AuthService();
