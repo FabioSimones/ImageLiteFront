e4da0e40e68d315ffa38b4af9e87361e48e98081
FabioSimones <simones.fabio1994@gmail.com>
2025-12-11 18:11:54 -0300

    Enviando o token Jwt via front end.

--- src/resources/image/image.service.ts
+++ src/resources/image/image.service.ts
@@ -1,18 +1,29 @@
 import { Image } from './image.resource'
+import { userAuth } from '@/resources'
 
 class ImageService{
     baseUrl: string = 'http://localhost:8080/v1/images';
+    auth = userAuth();
 
     async buscar(query: string = "", extension: string = "") : Promise<Image[]>{
+        const userSession = this.auth.getUserSession();
         const url = `${this.baseUrl}?query=${query}&extension=${extension}`
-        const response = await fetch(url);
+        const response = await fetch(url, {
+            headers: {
+                "Authorization": `Bearer ${userSession?.accessToken}`
+            }
+        });
         return await response.json();
     }
 
     async salvar(dados: FormData): Promise<string>{
+        const userSession = this.auth.getUserSession();
         const response = await fetch(this.baseUrl, {
             method: 'POST',
-            body: dados
+            body: dados,
+            headers: {
+                "Authorization": `Bearer ${userSession?.accessToken}`
+            }
         });
 
         return response.headers.get('location') ?? ''
--- src/resources/user/authentication.service.ts
+++ src/resources/user/authentication.service.ts
@@ -1,90 +1,98 @@
-import jwt from 'jwt-decode'
-import {AccessToken, Credentials, UserSessionToken, User} from './user.resource'
+import jwt from "jwt-decode";
+import {
+  AccessToken,
+  Credentials,
+  UserSessionToken,
+  User,
+} from "./user.resource";
 
-class AuthService{
-    baseURL: string = 'http://localhost:8080/v1/users';
-    static AUTH_PARAM: string = "_auth";
+class AuthService {
+  baseURL: string = "http://localhost:8080/v1/users";
+  static AUTH_PARAM: string = "_auth";
 
-    async authenticate(credentials : Credentials) : Promise<AccessToken>{
-        const response = await fetch(this.baseURL + "/auth", {
-            method: 'POST',
-            body: JSON.stringify(credentials),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        });
+  async authenticate(credentials: Credentials): Promise<AccessToken> {
+    const response = await fetch(this.baseURL + "/auth", {
+      method: "POST",
+      body: JSON.stringify(credentials),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-        if(response.status == 401){
-            throw new Error("User or password are incorrect!")
-        }
-
-        return await response.json();
+    if (response.status == 401) {
+      throw new Error("User or password are incorrect!");
     }
 
-    async save(user: User) : Promise<void>{
-        const response = await fetch(this.baseURL, {
-            method: 'POST',
-            body: JSON.stringify(user),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
+    return await response.json();
+  }
 
-        console.log("Response Auth.save: ", response)
+  async save(user: User): Promise<void> {
+    const response = await fetch(this.baseURL, {
+      method: "POST",
+      body: JSON.stringify(user),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-        if(response.status == 409){
-            const responseError = await response.json();
-            console.log("JSON ERROR: ", responseError)
-            throw new Error(responseError.Error)
-        }
+    console.log("Response Auth.save: ", response);
 
+    if (response.status == 409) {
+      const responseError = await response.json();
+      console.log("JSON ERROR: ", responseError);
+      throw new Error(responseError.Error);
+    }
+  }
+
+  initSession(token: AccessToken) {
+    if (token.accessToken) {
+      const decodedToken: any = jwt(token.accessToken);
+
+      console.log("DECODED TOKEN: ", decodedToken);
+
+      const userSessionToken: UserSessionToken = {
+        accessToken: token.accessToken,
+        email: decodedToken.sub,
+        name: decodedToken.name,
+        expiration: decodedToken.exp,
+      };
+
+      this.setUserSession(userSessionToken);
+    }
+  }
+
+  setUserSession(userSessionToken: UserSessionToken) {
+    localStorage.setItem(
+      AuthService.AUTH_PARAM,
+      JSON.stringify(userSessionToken)
+    );
+  }
+
+  getUserSession(): UserSessionToken | null {
+    if (typeof window === "undefined") {
+      return null;
     }
 
-    initSession(token : AccessToken){
-        if(token.accessToken){
-            const decodedToken: any = jwt(token.accessToken)
+    const authString = localStorage.getItem(AuthService.AUTH_PARAM);
+    if (!authString) return null;
 
-            console.log("DECODED TOKEN: ", decodedToken)
+    return JSON.parse(authString);
+  }
 
-            const userSessionToken: UserSessionToken= {
-                accessToken: token.accessToken,
-                email: decodedToken.sub,
-                name: decodedToken.name,
-                expiration: decodedToken.exp
-            }
-
-            this.setUserSession(userSessionToken)
-        }
+  isSessionValid(): boolean {
+    const userSession: UserSessionToken | null = this.getUserSession();
+    if (!userSession) {
+      return false;
     }
 
-    setUserSession (userSessionToken: UserSessionToken){
-        localStorage.setItem(AuthService.AUTH_PARAM, JSON.stringify(userSessionToken))
+    const expiration: number | undefined = userSession.expiration;
+    if (expiration) {
+      const expirationDateInMillis = expiration * 1000;
+      return new Date() < new Date(expirationDateInMillis);
     }
 
-    getUserSession() : UserSessionToken | null {
-        const authString = localStorage.getItem(AuthService.AUTH_PARAM);
-        if(!authString){
-            return null;
-        }
-
-        const token: UserSessionToken = JSON.parse(authString);
-        return token;
-    }
-
-    isSessionValid(): boolean{
-        const userSession: UserSessionToken | null =  this.getUserSession();
-        if(!userSession){
-            return false;
-        }
-
-        const expiration: number | undefined = userSession.expiration;
-        if(expiration){
-            const expirationDateInMillis = expiration * 1000;
-            return new Date() < new Date(expirationDateInMillis)
-        }
-        
-        return false;
-    }
+    return false;
+  }
 }
 
 export const userAuth = () => new AuthService();
